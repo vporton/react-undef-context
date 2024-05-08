5966327b23ca86fb5acb0cbfc58c45bad5dccc61
Victor Porton <porton.victor@gmail.com>
2024-05-08 16:09:37 +0300

    simplified, removed Bootstrap

--- frontend/src/component/App.tsx
+++ frontend/src/component/App.tsx
@@ -1,17 +1,13 @@
 import * as React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
 import EditFolder from "./EditFolder";
 import { ErrorBoundary } from "./ErrorBoundary";
  
 export default function App() {
     return (
         <>
-            <Container>
-                <ErrorBoundary>
-                    <EditFolder/>
-                </ErrorBoundary>
-            </Container>
+            <ErrorBoundary>
+                <p><EditFolder/></p>
+            </ErrorBoundary>
         </>
     );
 }
--- frontend/src/component/EditFolder.tsx
+++ frontend/src/component/EditFolder.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
 import { useContext } from "react";
-import { Button } from "react-bootstrap";
 import { ErrorContext } from "./ErrorBoundary";
 
 export default function EditFolder(props: {}) {
     const { setError } = useContext(ErrorContext);
-    async function submit() {
+    function submit() {
         setError("TEST EXCEPTION");
     }
     return (
-        <p><Button onClick={submit}>Save</Button></p>
+        <p><button onClick={submit}>Save</button></p>
     );
 }
