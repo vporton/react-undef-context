55b941250fdb0a2967780b851aefba008dadc30e
Victor Porton <porton.victor@gmail.com>
2024-05-08 15:57:47 +0300

    removed unused imports

--- src/frontend/src/component/EditFolder.tsx
+++ src/frontend/src/component/EditFolder.tsx
@@ -1,19 +1,7 @@
 import * as React from "react";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { Button } from "react-bootstrap";
-import { useNavigate, useParams } from "react-router-dom";
-import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
-import { Helmet } from 'react-helmet';
-import { idlFactory as mainIdlFactory } from "../../../declarations/main";
-import { ItemDataWithoutOwner, ItemTransferWithoutOwner, _SERVICE as ZonBackend } from "../../../declarations/main/main.did";
-import { idlFactory as canDBPartitionIdlFactory } from "../../../declarations/CanDBPartition";
-import { CanDBPartition } from "../../../declarations/CanDBPartition/CanDBPartition.did";
-import EditFoldersList from "./EditFoldersList";
-import { addToFolder, addToMultipleFolders } from "../util/folder";
-import { parseItemRef, serializeItemRef } from "../data/Data";
-import { AuthContext } from "./auth/use-auth-client";
-import { Actor, Agent } from "@dfinity/agent";
-import { ErrorContext, ErrorContextType } from "./ErrorBoundary";
+import { ErrorContext } from "./ErrorBoundary";
 
 export default function EditFolder(props: {}) {
     const errorContext = useContext(ErrorContext);
