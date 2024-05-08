5ca972e96a1b647b0bd0af4a433d6a198907cd11
Victor Porton <porton.victor@gmail.com>
2024-05-08 16:08:22 +0300

    simplified widget

--- frontend/src/component/EditFolder.tsx
+++ frontend/src/component/EditFolder.tsx
@@ -4,13 +4,9 @@ import { Button } from "react-bootstrap";
 import { ErrorContext } from "./ErrorBoundary";
 
 export default function EditFolder(props: {}) {
-    const errorContext = useContext(ErrorContext);
-    const setError = errorContext.setError;
+    const { setError } = useContext(ErrorContext);
     async function submit() {
-        async function submitItem() {
-            setError("TEST EXCEPTION");
-        }
-        await submitItem();
+        setError("TEST EXCEPTION");
     }
     return (
         <p><Button onClick={submit}>Save</Button></p>
